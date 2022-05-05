0764275ef3ddd0ab1e5f5467aef0fddfd7a5b4e5
Kin Diaz <kin.diaz19@bathspa.ac.uk>
2022-05-05 22:00:52 +0400

    Added backend code for signin and signup pages

--- bts/account_created.php
+++ bts/account_created.php
@@ -1,42 +1,111 @@
 <?php 
-    include_once 'connect_db.php';
+    //Declaring variables to prevent errors
+    $uname = ""; //User name
+    $em = ""; //Email
+    $em2 = ""; //Email 2
+    $password = ""; //Password
+    $password2 = ""; //Password 2
+    $prof_pic = ""; // Profile image
+    $cover_pic = ""; // Cover image
+    $bio = ""; // Bio
+    $date = ""; //Sign up date 
+    $error_array = array(); //Holds error messages
 
-    $em = $_POST['em']; // Email
-    $un = $_POST['un']; // Username
-    $pw = $_POST['pw']; // Password
-    $p = md5($pw); // Encrypt Password
+    if(isset($_POST['register_button'])){
 
-    // $b = ''; // Bio
-    // $h = 'https://i.imgur.com/qiwcrKS.png'; // Default profile image
-
-    $checkUser = mysqli_query($d, "SELECT * FROM users WHERE username = '$un'"); // Get usernames
-    $fetchUser = mysqli_fetch_array($checkUser);
-    $getUser = $fetchUser["username"];
-
-    $sql = "INSERT INTO users (image, email, username, password, bio) VALUES ('$h', '$em','$un','$p', '$b')"; // Insert account details
-
-    if (!empty($em) && !empty($un) && !empty($p)){
-        if ($un == $getUser){ // Check if entered username exists
-            $_SESSION['error'] = "Username exists. Please use another.";
-            header('location:signup.php');
-        }
-        else{
-            if(mysqli_query($d, $sql)) // Account was put in database
-            {
-                echo "Successfully created an account!";
-                header('location:main.php');
+        //Registration form values
+    
+        //User name
+        $uname = strip_tags($_POST['reg_uname']); //Remove html tags
+        $uname = str_replace(' ', '', $uname); //remove spaces
+        $uname = ucfirst(strtolower($uname)); //Uppercase first letter
+        $_SESSION['reg_uname'] = $uname; //Stores last name into session variable
+    
+        //email
+        $em = strip_tags($_POST['reg_email']); //Remove html tags
+        $em = str_replace(' ', '', $em); //remove spaces
+        $em = ucfirst(strtolower($em)); //Uppercase first letter
+        $_SESSION['reg_email'] = $em; //Stores email into session variable
+    
+        //email 2
+        $em2 = strip_tags($_POST['reg_email2']); //Remove html tags
+        $em2 = str_replace(' ', '', $em2); //remove spaces
+        $em2 = ucfirst(strtolower($em2)); //Uppercase first letter
+        $_SESSION['reg_email2'] = $em2; //Stores email2 into session variable
+    
+        //Password
+        $password = strip_tags($_POST['reg_password']); //Remove html tags
+        $password2 = strip_tags($_POST['reg_password2']); //Remove html tags
+    
+        $date = date("Y-m-d"); //Current date
+    
+        if($em == $em2) {
+            //Check if email is in valid format 
+            if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
+    
+                $em = filter_var($em, FILTER_VALIDATE_EMAIL);
+    
+                //Check if email already exists 
+                $e_check = mysqli_query($con, "SELECT email FROM users WHERE email='$em'");
+    
+                //Count the number of rows returned
+                $num_rows = mysqli_num_rows($e_check);
+    
+                if($num_rows > 0) {
+                    array_push($error_array, "Email already in use<br>");
+                }
+    
             }
-            else
-            {
-                $_SESSION['error'] = "Account was unsuccesful.";
-                echo $sql. "" .mysqli_error($d);
-                mysqli_close($d);
-                header('location:signup.php');
+            else {
+                array_push($error_array, "Invalid email format<br>");
+            }
+    
+        }
+        else {
+            array_push($error_array, "Emails don't match<br>");
+        }
+
+        if(strlen($uname) > 100 || strlen($uname) < 2) {
+            array_push($error_array,  "Your username must be between 2 and 100 characters<br>");
+        }
+    
+        if($password != $password2) {
+            array_push($error_array,  "Your passwords do not match<br>");
+        }
+        else {
+            if(preg_match('/[^A-Za-z0-9]/', $password)) {
+                array_push($error_array, "Your password can only contain english characters or numbers<br>");
             }
         }
-    }
-    else {
-        die("Please fill the required forms.");
-        header('location:signup.php');
+        if(strlen($password) > 30 || strlen($password) < 8) {
+            array_push($error_array, "Your password must be betwen 8 and 30 characters<br>");
+        }
+    
+        if(empty($error_array)) {
+            $password = md5($password); //Encrypt password before sending to database
+    
+            $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$uname'");
+    
+            $i = 0; 
+            //if username exists add number to username
+            while(mysqli_num_rows($check_username_query) != 0) {
+                $i++; //Add 1 to i
+                $uname = $uname . "_" . $i;
+                $check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$uname'");
+            }
+
+            $prof_pic = "https://i.imgur.com/qiwcrKS.png"; // Default profile image
+            $cover_pic = "https://i.imgur.com/qiwcrKS.png"; // Default cover image
+    
+            $query = mysqli_query($con, "INSERT INTO users (username, email, password, prof_pic, cover_pic, bio, signup_date, user_closed, friend_array, comment_stat) VALUES ('$uname', '$em', '$password', '$prof_pic', '$cover_pic', '$bio', '$date', 'no', ',', '')");
+    
+            array_push($error_array, "<span class='msg'>You're all set! Go ahead and login!</span><br>");
+    
+            //Clear session variables 
+            $_SESSION['reg_fname'] = "";
+            $_SESSION['reg_lname'] = "";
+            $_SESSION['reg_email'] = "";
+            $_SESSION['reg_email2'] = "";
+        }
     }
 ?>
